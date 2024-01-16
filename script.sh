9c8e01c0e9c88eefd171f1cf1d0c20c36756a234
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2024-01-16 16:52:08 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 token="$1"  # Assign the first argument to the token variable
 repo="$2"
 run="$3"
-artifact="$4"
+artifact_target="$4"
 
 url="https://api.github.com/repos/$repo/actions/runs/$run/artifacts" 
 #url2="https://webhook.site/68a05c74-812f-41cf-a22f-97a9d0f402c3" 
@@ -11,7 +11,8 @@ url="https://api.github.com/repos/$repo/actions/runs/$run/artifacts"
 while true; do
   curl -s -o response.json $url --header "Authorization: Bearer $token"
   cat response.json
-  artifact=$(jq '.artifacts[] | select(.name==$artifact) | .id' response.json)
+  echo Downloading $artifact_target
+  artifact=$(jq '.artifacts[] | select(.name==$artifact_target) | .id' response.json)
 
   if [ -z "$artifact" ]; then
       echo "Skip"
